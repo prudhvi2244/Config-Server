fd2c82f14e46035e485e5cbad41f7299b6dd5ffd
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 13:14:56 +0530

    First Commit

--- src/test/java/com/configserver/in/ConfigServerApplicationTests.java
+++ src/test/java/com/configserver/in/ConfigServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.configserver.in;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootTest
+@EnableConfigServer
 class ConfigServerApplicationTests {
 
 	@Test
